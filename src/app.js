b900730214cca1d489481d4ad53966b171ac1169
priyanshi gupta <gpriyanshi20052gmail.com>
2026-04-04 14:18:35 +0530

    all set

--- src/app.js
+++ src/app.js
@@ -31,7 +31,10 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json({ limit: '10kb' }));
 
 // Implement CORS
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  credentials: true
+}));
 
 // 2) ROUTES
 app.get('/', (req, res) => {
